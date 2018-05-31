b32a2cb9ff82df9ad5e210d8c02b238e9c880ab3
Delaroy Studios <bamoyk@yahoo.com>
2018-05-31 12:36:57 +0100

    video-upload

--- app/src/main/java/com/delaroystudios/uploadmedia/VideoActivity.java
+++ app/src/main/java/com/delaroystudios/uploadmedia/VideoActivity.java
@@ -2,13 +2,20 @@ package com.delaroystudios.uploadmedia;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.database.Cursor;
+import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.VideoView;
 
 import com.delaroystudios.uploadmedia.networking.ApiConfig;
 import com.delaroystudios.uploadmedia.networking.AppConfig;
@@ -29,15 +36,27 @@ import retrofit2.Response;
  */
 
 public class VideoActivity extends AppCompatActivity {
-    Button button;
+    private Button button, uploadVideo;
     public static final int REQUEST_PICK_VIDEO = 3;
     public ProgressDialog pDialog;
+    private VideoView mVideoView;
+    private TextView mBufferingTextView;
+    private Uri video;
+    private String videoPath;
+
+    // Current playback position (in milliseconds).
+    private int mCurrentPosition = 0;
+
+    // Tag for the instance state bundle.
+    private static final String PLAYBACK_TIME = "play_time";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.video_layout);
 
         button = (Button) findViewById(R.id.pickVideo);
+        uploadVideo = (Button) findViewById(R.id.uploadVideo);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -47,7 +66,105 @@ public class VideoActivity extends AppCompatActivity {
             }
         });
 
+        uploadVideo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (video != null){
+                    uploadFile();
+                }else{
+                    Toast.makeText(VideoActivity.this, "Please select a video", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        mVideoView = (VideoView) findViewById(R.id.videoview);
+        mBufferingTextView = (TextView) findViewById(R.id.buffering_textview);
+
+        if (savedInstanceState != null) {
+            mCurrentPosition = savedInstanceState.getInt(PLAYBACK_TIME);
+        }
+
+        // Set up the media controller widget and attach it to the video view.
+        MediaController controller = new MediaController(this);
+        controller.setMediaPlayer(mVideoView);
+        mVideoView.setMediaController(controller);
+
         initDialog();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+            mVideoView.pause();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        // Media playback takes a lot of resources, so everything should be
+        // stopped and released at this time.
+        releasePlayer();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        // Save the current playback position (in milliseconds) to the
+        // instance state bundle.
+        outState.putInt(PLAYBACK_TIME, mVideoView.getCurrentPosition());
+    }
+
+    private void initializePlayer(Uri uri) {
+        // Show the "Buffering..." message while the video loads.
+        mBufferingTextView.setVisibility(VideoView.VISIBLE);
+        if (uri != null){
+            mVideoView.setVideoURI(uri);
+        }
+        // Listener for onPrepared() event (runs after the media is prepared).
+        mVideoView.setOnPreparedListener(
+                new MediaPlayer.OnPreparedListener() {
+                    @Override
+                    public void onPrepared(MediaPlayer mediaPlayer) {
+
+                        // Hide buffering message.
+                        mBufferingTextView.setVisibility(VideoView.INVISIBLE);
+
+                        // Restore saved position, if available.
+                        if (mCurrentPosition > 0) {
+                            mVideoView.seekTo(mCurrentPosition);
+                        } else {
+                            // Skipping to 1 shows the first frame of the video.
+                            mVideoView.seekTo(1);
+                        }
+
+                        // Start playing!
+                        mVideoView.start();
+                    }
+                });
+
+        // Listener for onCompletion() event (runs after media has finished
+        // playing).
+        mVideoView.setOnCompletionListener(
+                new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mediaPlayer) {
+                        Toast.makeText(VideoActivity.this,
+                                R.string.toast_message,
+                                Toast.LENGTH_SHORT).show();
+
+                        // Return the video position to the start.
+                        mVideoView.seekTo(0);
+                    }
+                });
+    }
+
+    private void releasePlayer() {
+        mVideoView.stopPlayback();
     }
 
     @Override
@@ -60,8 +177,10 @@ public class VideoActivity extends AppCompatActivity {
                 if (data != null) {
                     Toast.makeText(this, "Video content URI: " + data.getData(),
                             Toast.LENGTH_LONG).show();
-                    Uri video = data.getData();
-                    uploadFile(video.getPath());
+                    video = data.getData();
+                    videoPath = getPath(video);
+                    initializePlayer(video);
+                   // uploadFile(video.getPath());
 
                 }
             }
@@ -71,8 +190,22 @@ public class VideoActivity extends AppCompatActivity {
         }
     }
 
-    private void uploadFile(String postPath) {
-        if (postPath == null || postPath.equals("")) {
+    public String getPath(Uri uri) {
+        String[] projection = { MediaStore.Video.Media.DATA };
+        Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
+        if (cursor != null) {
+            // HERE YOU WILL GET A NULLPOINTER IF CURSOR IS NULL
+            // THIS CAN BE, IF YOU USED OI FILE MANAGER FOR PICKING THE MEDIA
+            int column_index = cursor
+                    .getColumnIndexOrThrow(MediaStore.Video.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        } else
+            return null;
+    }
+
+    private void uploadFile() {
+        if (video == null || video.equals("")) {
             Toast.makeText(this, "please select an image ", Toast.LENGTH_LONG).show();
             return;
         } else {
@@ -80,8 +213,7 @@ public class VideoActivity extends AppCompatActivity {
 
             // Map is used to multipart the file using okhttp3.RequestBody
             Map<String, RequestBody> map = new HashMap<>();
-            File file = new File(postPath);
-
+            File file = new File(videoPath);
             // Parsing any Media type file
             RequestBody requestBody = RequestBody.create(MediaType.parse("*/*"), file);
             map.put("file\"; filename=\"" + file.getName() + "\"", requestBody);
@@ -92,12 +224,13 @@ public class VideoActivity extends AppCompatActivity {
                 public void onResponse(Call<ServerResponse> call, Response<ServerResponse> response) {
                     if (response.isSuccessful()){
                         if (response.body() != null){
-                            showpDialog();
+                            hidepDialog();
                             ServerResponse serverResponse = response.body();
                             Toast.makeText(getApplicationContext(), serverResponse.getMessage(), Toast.LENGTH_SHORT).show();
 
                         }
                     }else {
+                        hidepDialog();
                         Toast.makeText(getApplicationContext(), "problem uploading image", Toast.LENGTH_SHORT).show();
                     }
                 }
